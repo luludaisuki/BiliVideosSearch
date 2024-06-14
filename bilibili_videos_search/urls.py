f1bacdd808fac9a67d757fcfecd06a65869dddf2
luludaisuki <153270717+luludaisuki@users.noreply.github.com>
2024-06-14 18:25:32 +0800

    add url file

---
+++ bilibili_videos_search/urls.py
@@ -0,0 +1,11 @@
+get_tag='https://api.bilibili.com/x/tag/archive/tags'
+get_author_video='https://api.bilibili.com/x/space/wbi/arc/search'
+get_series_archives='https://api.bilibili.com/x/series/archives'
+get_related_video='https://api.bilibili.com/x/web-interface/archive/related'
+get_video='https://api.bilibili.com/x/web-interface/view'
+get_video_stats='https://api.bilibili.com/x/web-interface/archive/stat'
+set_proxy_global='http://127.0.0.1:9090/proxies/GLOBAL'
+get_proxy='http://127.0.0.1:9090/proxies'
+get_dynamics='https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space'
+
+
